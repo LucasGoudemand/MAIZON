78a8f340ca7c082c83650821caaeba4775c9c197
Lucas Goudemand <goudemand.lucas@gmail.com>
2023-06-02 09:52:47 +0200

    Fixing id url issue and flex issue

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -14,9 +14,11 @@ flex-direction:column;
 margin-left:80px;
 margin-right:80px;
 margin-top:50px;
+height:100vh;
 @media (min-width: 375px) and (max-width: 767px) {
   margin-left:0px;
 margin-right:0px;
+
 }
 `;
 
@@ -30,25 +32,27 @@ function Apropos() {
         <div className="ContainerMain">
           <Header />
           <section>
-            <Banner backgroundImage={background} Title="A Propos" />
-            <DetailsContainer>
-              <DetailsBlock
-                Title="Fiabilité"
-                Text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-              />
-              <DetailsBlock
-                Title="Respect"
-                Text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-              />
-              <DetailsBlock
-                Title="Service"
-                Text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-              />
-              <DetailsBlock
-                Title="Sécurité"
-                Text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-              />
-            </DetailsContainer>
+            <div className="containerApropos">
+              <Banner backgroundImage={background} Title="A Propos" />
+              <DetailsContainer>
+                <DetailsBlock
+                  Title="Fiabilité"
+                  Text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+                />
+                <DetailsBlock
+                  Title="Respect"
+                  Text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+                />
+                <DetailsBlock
+                  Title="Service"
+                  Text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+                />
+                <DetailsBlock
+                  Title="Sécurité"
+                  Text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+                />
+              </DetailsContainer>
+            </div>
           </section>
         </div>
         <Footer />
--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -9,10 +9,21 @@ import DescriptionDetailled from "../components/description/descriptionDetailled
 import logements from "../api/logements.json";
 import { useParams } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function FicheLogement(props) {
   let { id } = useParams();
   const appartement = logements.find((logement) => logement.id === id);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (appartement === undefined) {
+      // Si la variable appartement renvoi undefined (elle n'a pas trouvé l'id) alors redirection vers /error
+      navigate("/error");
+    }
+  }, [appartement, navigate]);
 
   return (
     <HelmetProvider>
@@ -23,9 +34,16 @@ function FicheLogement(props) {
         <div className="ContainerMain">
           <Header />
           <section>
-            <Slider logements={appartement} />
-            <Description logements={appartement} />
-            <DescriptionDetailled logements={appartement} />
+            {appartement ? ( //Si il trouve l'id alors il affiche le slider "?"
+              <>
+                <Slider logements={appartement} />
+                <Description logements={appartement} />
+                <DescriptionDetailled logements={appartement} />
+              </>
+            ) : (
+              //sinon il affiche cette div ":"
+              <div>Logement introuvable</div>
+            )}
           </section>
         </div>
         <Footer />
